a605e3d5fb44552a265d6a73ae984299c9cad729
vishwa <vw1520@gmail.com>
2023-07-23 22:44:56 +0530

    Create python_List.py

---
+++ python_List.py
@@ -0,0 +1,68 @@
+list1=[1,2,3,4,5]
+result=list1
+print(result)
+
+print(list1)
+
+
+list2=(1,2,3,5)
+print(list2)
+
+l3=[]
+l3.append(1)
+l3.append(6)
+# l3.extend(2)
+print(l3)
+
+# l4=[]
+# for num in range(1,101):
+#     l4.append(num)
+
+
+int_list = []
+for num in range(1,1001):
+    int_list.append(num)
+
+len_list=len(list2)
+print(len_list)
+
+
+
+
+list3=[1,"hi vishwa",30]
+list4=[1,30,"hi vishwa"]
+print('equal =' ,list3==list4)
+
+
+
+list1 = [1,"Shashank",20,"Hi"]
+list2 = [1,"Shashank",20,"Hi"]
+print("Lists are equal ?? ", list1 == list2)
+
+
+#concat line
+list8=[1,2,4,8,9]
+list9=[50,5,8,6,8]
+
+result=list8+list9
+print(result)
+
+list4 = [1,2,3,4,5]
+list5 = [80,90,100,110]
+result = list4 + list5
+print(result)
+
+l5=[10,20,30,50,60,70]
+
+for i in l5:
+    print(i)
+
+
+print(l5[2:5])
+
+l5=[1,"vishwa ",30]
+print(l5)
+l5[1]="wase"
+print(l5)
+
+
